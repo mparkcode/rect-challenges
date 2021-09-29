e1b32f76a1f75a2ebce9fff516754beb022f23b6
Michael Park <mparkcode@gmail.com>
2021-09-29 07:01:40 +0000

    blah

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,10 @@
 import css from './App.module.css';
-import Sidebar from './components/Sidebar';
+import StatefulGreeting from './components/StatefulGreeting';
 
 function App() {
   return (
     <div className={css.App}>
-      <Sidebar />
+      <StatefulGreeting greeting="I'm stateful"/>
     </div>
   );
 }
